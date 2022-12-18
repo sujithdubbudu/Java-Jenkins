2a4938dc1994332c8a889ffeaa21369e65ccfb1f
sujith <sujithdubbudu@gmail.com>
2022-12-17 20:21:26 -0500

    added swagger config

--- src/main/java/com/example/demo/controller/Sample.java
+++ src/main/java/com/example/demo/controller/Sample.java
@@ -11,7 +11,28 @@ public class Sample {
 	@GetMapping("/")
 	String Test()
 	{
-		return "Jenkins Build Test";
+		return "Docker  Build Test";
 	}
+	
+	@GetMapping("/camelCase")
+	String myCamelCase()
+	{
+		return "String test= \"a b c d-e-f%g\";\r\n"
+				+ "		String[] words = test.split(\"[\\\\W_]+\");\r\n"
+				+ "		StringBuilder builder = new StringBuilder();\r\n"
+				+ "		for (int i = 0; i < words.length; i++) {\r\n"
+				+ "			String word = words[i];\r\n"
+				+ "			if (i == 0) {\r\n"
+				+ "				word = word.isEmpty() ? word : word.toLowerCase();\r\n"
+				+ "			} else {\r\n"
+				+ "				word = word.isEmpty() ? word : Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase();\r\n"
+				+ "			}\r\n"
+				+ "			builder.append(word);\r\n"
+				+ "		}\r\n"
+				+ "		System.out.println(builder.toString());";
+	}
+	
+	
+	
 
 }
