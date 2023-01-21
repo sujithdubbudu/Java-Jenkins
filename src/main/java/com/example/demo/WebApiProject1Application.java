28ffc023beaa872360f0c46e5ab9250efc0feb86
sujith <sujithdubbudu@gmail.com>
2023-01-21 18:15:13 -0500

    added eureka and config server

--- src/main/java/com/example/demo/WebApiProject1Application.java
+++ src/main/java/com/example/demo/WebApiProject1Application.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableEurekaClient
 public class WebApiProject1Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/controller/Sample.java
+++ src/main/java/com/example/demo/controller/Sample.java
@@ -1,12 +1,9 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,8 +23,10 @@ public class Sample {
 	@Autowired
 	UserService userService;
 	
-	@Value("${spring.application.owner}")
-	private String ownerName;
+	
+	 @Value("${spring.application.owner}")
+	 private String ownerName;
+	 
 	
 	@GetMapping("/")
 	String Test()
